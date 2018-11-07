41319b3f5a5d2e9c4af2dbedb8f1000f298e1e88
Alexr Bystritskiy <oleksandr.bystrytskyi@sixt.com>
2018-11-07 16:16:56 +0100

    algorithms week2 - queue/part2

--- src/main/java/Deque.java
+++ src/main/java/Deque.java
@@ -1,102 +1,63 @@
 import java.util.*;
-import java.io.*;
 import java.lang.*;
-import edu.princeton.cs.algs4.StdRandom;
 
 public class Deque<Item> implements Iterable<Item> {
-    private Item[] s;
     private int size;
-    private int first;
-    private int last;
+    private Node first, last;
 
     public Deque() {
-        size = 1;
-        s = (Item[]) new Object[size];
-        first = 0;
-        last = 0;
+        this.size = 0;
     }
 
     public boolean isEmpty() {
-        return s[first] == null;
+        return this.size == 0;
     }
 
     public int size() {
-        if (isEmpty()) {
-            return  0;
-        }
-        return last - first + 1;
+        return this.size;
     }
 
     public void addFirst(Item item) {
-        if (last + 1 > size) {
-            resize(2 * size);
-        }
-        Item[] copy  = (Item[]) new Object[size];
-        copy[first] = item;
-
-        for (int i = first; i<=last; i++) {
-            copy[i+1] = s[i];
-        }
-        s = copy;
-        last++;
+        Node oldFirst = this.first;
+        Node newFIrst = new Node()
+        this.first = item;
+        this.first.next = oldFirst;
+        this.size++;
     }
 
     public void addLast(Item item) {
-        if (last + 1 > size) {
-            resize(2 * size);
-        }
-        s[last] = item;
-        last++;
+
     }
 
     public Item removeFirst() {
-        if (this.isEmpty()) {
-            throw new NoSuchElementException();
-        }
-        Item item = s[first];
-        first++;
-        return item;
+
     }
 
     public Item removeLast() {
-        if (this.isEmpty()) {
-            throw new NoSuchElementException();
-        }
-        Item item = s[last];
-        s[last] = null;
-        last--;
-        if (last < size / 4) {
-            resize(size / 2);
-        }
 
-        return item;
     }
 
     public Iterator<Item> iterator() {
         return new DequeueIterator();
     }
 
-    public static void main(String[] args) {
-        // unit testing (optional)
+    private class Node {
+        private Node next, prev;
+        private Item value;
+
+        protected void Node(Item item) {
+            this.value = item;
+        }
+
     }
 
-    private void resize(int c) {
-        Item[] copy = (Item []) new Object[c];
-        for (int i = first; i <= last; i++) {
-            copy[i] = s[i];
-        }
-        size = c;
-        s = copy;
-        first = 0;
-        last = size - 1;
-    }
 
     private class DequeueIterator implements Iterator<Item>
     {
         private int current = 0;
 
         public boolean hasNext() {
-            return current != last;
+//            return
         }
 
         public Item next() {
--- src/main/java/RandomizedQueue.java
+++ src/main/java/RandomizedQueue.java
@@ -1,44 +1,102 @@
 import java.util.*;
-import java.io.*;
 import java.lang.*;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
     private Item[] s;
     private int size;
-    private int first;
-    private int last;
 
     public RandomizedQueue() {
-        size = 1;
-        s = (Item[]) new Object[size];
-        first = 0;
-        last = 0;
+        this.size = 0;
+        this.s = (Item[]) new Object[1];
     }
+
     public boolean isEmpty() {
-        return s[first] == null;
+        return this.size == 0;
     }
+
     public int size() {
-        if (this.isEmpty()) {
-            return 0;
-        }
-        return last - first + 1;
+        return this.size;
     }
+
     public void enqueue(Item item) {
-        if (last + 1 > size) {
-            resize
+        if (item == null) {
+            throw new IllegalArgumentException();
         }
+        if (this.size == this.s.length) {
+            this.resize(this.s.length * 2);
+        }
+        this.size++;
+        s[this.size] = item;
     }
+
     public Item dequeue() {
-        // remove and return a random item
+        if (this.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        int id = StdRandom.uniform(size);
+        Item item = this.s[id];
+        if (id != this.size-1) {
+            this.s[id] = this.s[this.size];
+            this.s[this.size] = null;
+        }
+
+        if (this.size < this.s.length / 4) {
+            this.resize(this.s.length / 2);
+        }
+
+        this.size--;
+        return item;
     }
+
+    protected void resize(int capacity)
+    {
+        Item[] copy = (Item []) new Object[capacity];
+        for (int i = 0; i < capacity; i++) {
+            copy[i] = s[i];
+        }
+        this.s = copy;
+    }
+
+
     public Item sample() {
-        // return a random item (but do not remove it)
+        if (this.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        int id = StdRandom.uniform(size);
+        return s[id];
     }
-    public Iterator<Item> iterator() {
-        // return an independent iterator over items in random order
-    }
+
+
     public static void main(String[] args) {
         // unit testing (optional)
     }
+
+    public Iterator<Item> iterator() {
+        return new DequeueIterator();
+    }
+
+    private class DequeueIterator implements Iterator<Item>
+    {
+        private int current = 0;
+
+        public boolean hasNext() {
+            return this.current != RandomizedQueue.this.s.length;
+        }
+
+        public void remove(Item item) {
+            throw new UnsupportedOperationException();
+        }
+
+        public Item next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            Item item = RandomizedQueue.this.s[this.current];
+            this.current++;
+            return item;
+        }
+    }
+
 }
