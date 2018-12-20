2a167657861d2bcd599fc4f83b1c395a9b8bbd78
Alex Bystritskiy <alex.bystritskiy@gmail.com>
2018-12-20 16:55:27 +0100

    algs2: week 4 - submission 1

--- src/main/boggle/BoggleSolver.java
+++ src/main/boggle/BoggleSolver.java
@@ -159,5 +159,25 @@ public class BoggleSolver {
 
     public static void main(String[] args) {
         // unit tests
+
+        String[] dictionary = new String[6013];
+        edu.princeton.cs.algs4.In in = new edu.princeton.cs.algs4.In(/*"input/boggle/dictionary-zingarelli2005.txt"*/);
+        for (int i = 0; i < 6013; i++) {
+            dictionary[i] = in.readLine().trim();
+        }
+        BoggleSolver bs = new BoggleSolver(dictionary);
+        BoggleBoard bb = new BoggleBoard("input/boggle/board-q.txt");
+
+
+        Iterable<String> words = bs.getAllValidWords(bb);
+        ArrayList<String> sorted = new ArrayList<>();
+        for (String word : words) {
+            sorted.add(word);
+        }
+
+        java.util.Collections.sort(sorted);
+        for (String word : sorted) {
+            System.out.println(word);
+        }
     }
 }
