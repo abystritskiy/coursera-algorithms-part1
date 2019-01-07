4c1a75365a64e624544428fe5f5ce807cdabccbd
Alex Bystritskiy <alex.bystritskiy@gmail.com>
2019-01-07 23:46:34 +0100

    course 2/week 5/submission move to front

--- src/main/burrows/MoveToFront.java
+++ src/main/burrows/MoveToFront.java
@@ -1,15 +1,10 @@
-import edu.princeton.cs.algs4.HexDump;
-import edu.princeton.cs.algs4.BinaryStdIn;
-import edu.princeton.cs.algs4.BinaryStdOut;
-import edu.princeton.cs.algs4.StdOut;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-
 /*
-java -classpath "W:\stuff\Java\percolation\out\production\percolation;W:\stuff\Java\percolation\lib\algs4.jar" MoveToFront - < W:\stuff\Java\percolation\input\burrows\abra.txt
 https://theasciicode.com.ar/
+
+windows
+java -classpath "W:\stuff\Java\percolation\out\production\percolation;W:\stuff\Java\percolation\lib\algs4.jar" MoveToFront - < W:\stuff\Java\percolation\input\burrows\abra.txt | java -classpath "W:\stuff\Java\percolation\out\production\percolation;W:\stuff\Java\percolation\lib\algs4.jar" edu.princeton.cs.algs4.HexDump 16
+java -classpath "W:\stuff\Java\percolation\out\production\percolation;W:\stuff\Java\percolation\lib\algs4.jar" MoveToFront - < W:\stuff\Java\percolation\input\burrows\abra.txt | java -classpath "W:\stuff\Java\percolation\out\production\percolation;W:\stuff\Java\percolation\lib\algs4.jar" MoveToFront +
+
 
 MAC:
 cd /Users/o9834/work/Java/coursera-algorithms-part1/src/main/burrows
@@ -17,33 +12,39 @@ javac -classpath /Users/o9834/work/Java/coursera-algorithms-part1/out/production
 java -classpath /Users/o9834/work/Java/coursera-algorithms-part1/out/production/coursera-algorithms-part1:/Users/o9834/work/Java/coursera-algorithms-part1/lib/algs4.jar  MoveToFront  - < /Users/o9834/work/Java/coursera-algorithms-part1/input/burrows/abra.txt | java -cp "/Users/o9834/work/Java/coursera-algorithms-part1/lib/algs4.jar" edu.princeton.cs.algs4.HexDump 16
 
 java -classpath /Users/o9834/work/Java/coursera-algorithms-part1/out/production/coursera-algorithms-part1:/Users/o9834/work/Java/coursera-algorithms-part1/lib/algs4.jar  MoveToFront  - < /Users/o9834/work/Java/coursera-algorithms-part1/input/burrows/abra.txt | java -cp "/Users/o9834/work/Java/coursera-algorithms-part1/out/production/coursera-algorithms-part1:/Users/o9834/work/Java/coursera-algorithms-part1/lib/algs4.jar" MoveToFront +
- */
+*/
+
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class MoveToFront {
-    private static byte[] charTable;
-    private static HashMap<Byte, Integer> charMap;
     private final static int RADIX = 256;
+    private static int[] charTable;
+    private static HashMap<Integer, Integer> charMap;
 
     public static void encode() {
         ArrayList<Integer> out = new ArrayList<>();
 
-        charTable = new byte[RADIX];
+        charTable = new int[RADIX];
         charMap = new HashMap<>();
 
-        for (int i=0; i<RADIX; i++) {
-            charTable[i] = (byte) (i);
-            charMap.put((byte) (i), i) ;
+        for (int i = 0; i < RADIX; i++) {
+            charTable[i] = i;
+            charMap.put(i, i);
         }
-
 
         while (!BinaryStdIn.isEmpty()) {
-            byte c = BinaryStdIn.readByte();
+            int c = (int) BinaryStdIn.readByte();
             int position = charMap.get(c);
             out.add(position);
-            rebuildMap(position);
+            rebuildMapFor(position);
         }
 
-        for (Integer chr: out) {
+        for (Integer chr : out) {
             BinaryStdOut.write(chr, Byte.SIZE);
         }
 
@@ -51,55 +52,48 @@ public class MoveToFront {
     }
 
     public static void decode() {
-        System.out.println("decode!");
-        // apply move-to-front decoding, reading from standard input and writing to standard output
         ArrayList<Character> out = new ArrayList<>();
 
-        charTable = new byte[RADIX];
+        charTable = new int[RADIX];
         charMap = new HashMap<>();
 
-        for (int i=0; i<RADIX; i++) {
-            charTable[i] = (byte) (i);
-            charMap.put((byte) (i), i) ;
+        for (int i = 0; i < RADIX; i++) {
+            charTable[i] = i;
+            charMap.put(i, i);
         }
-//        byte[] seq = new byte[] {41, 42, 52, 02, 44, 01, 45, 01, 04, 04, 02, 26};
+
         while (!BinaryStdIn.isEmpty()) {
-            byte c = BinaryStdIn.readByte();
-            int position = charMap.get(c);
-            out.add((char) position);
-            rebuildMap(position);
+            byte b = BinaryStdIn.readByte();
+            int c = (int) b;
+            out.add((char) charTable[c]);
+            rebuildMapFor(c);
         }
 
-        for (Character chr: out) {
-            System.out.print(chr);
+        for (Character chr : out) {
+            BinaryStdOut.write(chr);
         }
-
         BinaryStdOut.close();
     }
 
-
-    private static void printMap() {
-        for (Byte j: charTable) {
+    private static void printMap(int position) {
+        for (int j : charTable) {
             System.out.print(j + " ");
         }
         System.out.println("\n");
     }
 
-
-    private static void rebuildMap(int pos) {
-        byte found = charTable[pos];
-        for (int i = pos-1; i >= 0; i--) {
-            charTable[i+1] = charTable[i];
-            charMap.put(charTable[i+1], i+1);
+    private static void rebuildMapFor(int pos) {
+        int found = charTable[pos];
+        for (int i = pos - 1; i >= 0; i--) {
+            charTable[i + 1] = charTable[i];
+            charMap.put(charTable[i + 1], i + 1);
         }
 
         charTable[0] = found;
         charMap.put(found, 0);
     }
 
-
     public static void main(String[] args) {
-
         if (args.length > 0) {
             if (args[0].equals("-")) {
                 encode();
